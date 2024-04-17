7e0bc4139a21e65d98b65bd8c3fe2c8e02c765ec
Kevinmadrid12 <100392602+Kevinmadrid12@users.noreply.github.com>
2024-04-17 12:44:19 -0600

    Add files via upload

--- buscador.php
+++ buscador.php
@@ -6,22 +6,22 @@ $search = $_GET['search'];
 
 // Construir la consulta SQL según la búsqueda y la ubicación seleccionada
 if (!empty($search) && !empty($location)) {
-    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location
+    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location, l.address, l.city
                 FROM coworking_space c
                 INNER JOIN location l ON c.location_id = l.location_id
                 WHERE (c.descripcion LIKE '%$search%' OR c.space_name LIKE '%$search%') AND l.location_id = $location";
 } elseif (!empty($search)) {
-    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location
+    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location, l.address, l.city
                 FROM coworking_space c
                 INNER JOIN location l ON c.location_id = l.location_id
                 WHERE c.descripcion LIKE '%$search%' OR c.space_name LIKE '%$search%'";
 } elseif (!empty($location)) {
-    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location
+    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location, l.address, l.city
                 FROM coworking_space c
                 INNER JOIN location l ON c.location_id = l.location_id
                 WHERE l.location_id = $location";
 } else {
-    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location
+    $query = "SELECT c.coworking_space_id, c.space_name, c.descripcion, c.Precio, l.name_location, l.address, l.city
                 FROM coworking_space c
                 INNER JOIN location l ON c.location_id = l.location_id";
 }
@@ -36,8 +36,8 @@ while ($fila = mysqli_fetch_assoc($result)) {
         <div class='card-body'>
             <h5 class='card-title'>Nombre: {$fila['space_name']}</h5>
             <p class='card-text'>Descripción: {$fila['descripcion']}</p>
-            <p class='card-text'><strong>Precio:</strong> {$fila['Precio']}</p>
-            <p class='card-text'><strong>Ubicación:</strong> {$fila['name_location']}</p>
+            <p class='card-text'><strong>Precio: $</strong>{$fila['Precio']}</p>
+            <p class='card-text'><strong>Ubicación:</strong> {$fila['name_location']} , {$fila['address']} , {$fila['city']}</p>
         </div>
     </div>
 </div>";
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
     cargarTarjetas();
-    $("#search").on("input", function() {
-        var searchValue = $(this).val().trim();
+    
+    // Función para manejar el evento de clic en el botón de búsqueda
+    $("button.btn-outline-success").click(function() {
+        var searchValue = $("#search").val().trim();
         if (searchValue !== "") {
             buscar(searchValue);
         } else {
+            alert("Ingrese un término de búsqueda válido.");
+        }
+    });
+
+    // Función para manejar el evento de entrada en el input de búsqueda
+    $("#search").on("input", function() {
+        var searchValue = $(this).val().trim();
+        if (searchValue === "") {
             cargarTarjetas();
         }
     });
